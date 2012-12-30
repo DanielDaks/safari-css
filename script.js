51768bc22c74c15ea012d60b10bd41aead042537
Jonathan Friedman <friedmanj98@gmail.com>
2012-12-29 20:01:06 -0500

    Checkpoint: saving for daks

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-jQuery(function($) {
-  $('.tile').click(function() {
-    var active = $('.tile.active');
-
-    active.removeClass('active');
-
-    /* If this is the active tile, then give it a "close" behaviour. */
-    if (active[0] !== this) {
-      $(this).addClass('active');
-    }
+  $(document).ready(function() {
+    $('.tile').click(function() {
+      var active = $('.tile.active');
+      active.removeClass('active');
+      /* If this is the active tile, then give it a "close" behaviour. */
+      if (active[0] !== this) {
+        $(this).addClass('active');
+      }
+    });
   });
-});
